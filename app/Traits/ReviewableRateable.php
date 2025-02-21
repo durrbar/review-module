82b778c77fb01d36a6e585db677b842af12d8646
Kid Max <kidmax285@gmail.com>
2025-02-21 10:02:46 +0600

    first commit

---
+++ app/Traits/ReviewableRateable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\Review\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Modules\Review\Models\Review;
+
+trait ReviewableRateable
+{
+    /**
+     * Return the reviews relationship.
+     */
+    public function reviews(): MorphMany
+    {
+        return $this->morphMany(Review::class, 'reviewable');
+    }
+
+    public function ratings(): array
+    {
+        $ratings = [];
+
+        for ($i = 5; $i >= 1; $i--) {
+            $ratings[] = [
+                'name' => $i . 'star',
+                'count' => $this->reviews()->where('rating', '=', $i)->count()
+            ];
+        }
+
+        return $ratings;
+    }
+
+    /**
+     * Calculate the average rating.
+     */
+    public function averageRating(): float
+    {
+        return $this->reviews()->avg('rating') ?? 0.0;
+    }
+}
