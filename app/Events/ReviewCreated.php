7f38ee2dc4590c8e7363de444e0ddcb41cd2f958
Kid Max <kidmax285@gmail.com>
2026-04-03 17:56:06 +0600

    refactor(events): use constructor property promotion for review created event

--- app/Events/ReviewCreated.php
+++ app/Events/ReviewCreated.php
@@ -8,13 +8,8 @@ use Modules\Review\Models\Review;
 
 class ReviewCreated
 {
-    public $review;
-
     /**
      * Create a new event instance.
      */
-    public function __construct(Review $review)
-    {
-        $this->review = $review;
-    }
+    public function __construct(public Review $review) {}
 }
